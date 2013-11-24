b02ab01e25a7b60ab429f4b8e64bea8b9510221f
Ken Barber <ken@bob.sh>
2013-11-24 02:26:04 +0100

    no persistent udev net rules for fedora19
    Signed-off-by: Ken Barber <ken@bob.sh>

--- templates/fedora-19-x64/scripts/cleanup.sh
+++ templates/fedora-19-x64/scripts/cleanup.sh
@@ -3,4 +3,3 @@ yum -y clean all
 
 # Remove traces of mac address from network configuration
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
-rm /etc/udev/rules.d/70-persistent-net.rules
